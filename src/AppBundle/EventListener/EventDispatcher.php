c0baf0fe44b8903ac00029c8efc3b6dccf74ce6a
Yosrabej <79087257+Yosrabej@users.noreply.github.com>
2021-10-29 12:00:20 +0100

    Update EventDispatcher.php

--- src/AppBundle/EventListener/EventDispatcher.php
+++ src/AppBundle/EventListener/EventDispatcher.php
@@ -1,6 +1,16 @@
 <?php
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-class EventDispatcher
+class EventDispat
 {
+    public function Dispatcher()
+    {
+        $dispatcher = new EventDispatcher;
+        $dispatcher->addListener('a file created', function () {
+            
+            $test = new FileSystemImproved();
+            $test->writeInFile('history', 'msg: a file has been created');
+        }
+    }
 }
